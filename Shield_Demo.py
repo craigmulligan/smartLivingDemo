a4ac8afa56cd34bb1f730c2434552a4dc5c1bda1
Craig Mulligan <craig@firingsuad.co.za>
2015-05-15 14:52:18 +0100

    fix envar fallback

--- Shield_Demo.py
+++ Shield_Demo.py
@@ -18,11 +18,11 @@ IOT.ClientKey = os.getenv('CLIENTKEY')
 #and to create a unique assetId which is a combination of deviceID+Pin number. The Pin number can be any value between (0 - 2^63)
 
 sensorName = "Button"            		    #name of the sensor
-sensorPin = int(os.getenv('BUTTONPIN')) || 2
+sensorPin = int(os.getenv('BUTTONPIN', '1'))
 sensorPrev = False                                  #previous value of the sensor (only send a value when a change occured)
 
 actuatorName = "Diode"
-actuatorPin = int(os.getenv('LEDPIN')) || 4
+actuatorPin = int(os.getenv('LEDPIN', '4'))
 
 #set up the pins
 grovepi.pinMode(sensorPin,"INPUT")
